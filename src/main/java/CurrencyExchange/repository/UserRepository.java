d9bd008db4ba68f29d9aae516290bcb1a6d6f1f1
ivan_ochc <sponsme@ukr.net>
2015-03-19 11:32:53 +0200

    delete commented lines

--- src/main/java/CurrencyExchange/repository/UserRepository.java
+++ src/main/java/CurrencyExchange/repository/UserRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.repository.CrudRepository;
 import CurrencyExchange.model.User;
 
 public interface UserRepository extends CrudRepository<User, Integer> {
-//    User findByEmail(String email);
     User findByName(String name);
 
 }
