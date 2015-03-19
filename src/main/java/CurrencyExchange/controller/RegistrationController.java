40fe097fd8dda15a5c12fbdbf97312f054ae64d4
ivan_ochc <sponsme@ukr.net>
2015-03-19 11:33:11 +0200

    delete commented lines

--- src/main/java/CurrencyExchange/controller/RegistrationController.java
+++ src/main/java/CurrencyExchange/controller/RegistrationController.java
@@ -7,16 +7,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.validation.Valid;
 
 @Controller
-//@RequestMapping("/registration")
 public class RegistrationController {
 
     private Role role;
@@ -34,35 +32,21 @@ public class RegistrationController {
         return "registration";
     }
 
+    @ResponseBody
     @RequestMapping(value = "/add", method = RequestMethod.POST)
-    public String addUser(@ModelAttribute("user") @Valid User user, BindingResult result, RedirectAttributes redirectAttributes, Model model)
-    {
-
-        if (result.hasErrors()) {
-            redirectAttributes.addFlashAttribute("org.springframework.validation.BindingResult.user", result);
-            redirectAttributes.addFlashAttribute("user", user);
-            return "redirect:/registration";
-        }
+    public String addUser(@ModelAttribute("user") @Valid User user){
 
         String userName = userManager.getUser(user);
 
         if (userName == null) {
             user.setEnabled("true");
             user.setRole(role.ROLE_USER);
-//            if (!user.getPassword().equals(user.getConfirmPassword())) {
-////                redirectAttributes.addFlashAttribute("pwdMessage", "Passwords are different");
-//                return "redirect:/registration?passwordError";
-//            }
             userManager.saveUser(user);
-            model.addAttribute("success", "success");
-//            redirectAttributes.addFlashAttribute("userMessage", "User was successfully created");
-            return "redirect:/registration";
+            return "success";
         }
         else
         {
-            model.addAttribute("NameError", "nameError");
-        //    redirectAttributes.addFlashAttribute("errorMessage", "This user name has already used. Please, try another one");
-            return "redirect:/registration?nameError";
+            return "nameError";
         }
 
 
