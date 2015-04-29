0e1619901b07ac63f1a182c8ccb2909afa243050
ivan_ochc <sponsme@ukr.net>
2015-04-29 13:46:08 +0300

    delete some methods

--- src/main/java/CurrencyExchange/service/OrderService.java
+++ src/main/java/CurrencyExchange/service/OrderService.java
@@ -3,19 +3,15 @@ package currencyexchange.service;
 import currencyexchange.model.Order;
 import currencyexchange.vo.OrdersListVO;
 
-import java.util.List;
-
 public interface OrderService {
 
     public void addOrder(Order order);
 
-    public List<Order> getOrdersList(String userName);
+//    public List<Order> getOrdersList(String userName);
 
     public void removeOrder(Order order);
 
-    public long getSumOfAllCosts(String userName);
-
-    public Order getOrder(Integer recordId);
+    public Order getOrder(int recordId);
 
     public void updateOrder(Order order);
 
