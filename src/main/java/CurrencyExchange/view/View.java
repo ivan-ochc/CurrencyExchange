bfbac7334101717f527fde518afb10b62a065954
ivan_ochc <sponsme@ukr.net>
2015-04-29 13:34:57 +0300

    add views for json

---
+++ src/main/java/CurrencyExchange/view/View.java
@@ -0,0 +1,9 @@
+package currencyexchange.view;
+
+public class View {
+
+    public interface Name {}
+    public interface Secure extends Name {}
+    public static class Public implements Name {}
+
+}
