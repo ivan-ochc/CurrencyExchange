237b6a018508dfa2fe34ec39f24c4d2a7bf4b38d
ivan_ochc <sponsme@ukr.net>
2015-04-29 13:47:24 +0300

    add json view

--- src/main/java/CurrencyExchange/vo/TransactionsListVO.java
+++ src/main/java/CurrencyExchange/vo/TransactionsListVO.java
@@ -1,19 +1,25 @@
 package currencyexchange.vo;
 
+import com.fasterxml.jackson.annotation.JsonView;
+import currencyexchange.model.ExchangeTransaction;
+import currencyexchange.view.View;
+
 import java.util.List;
 
 public class TransactionsListVO {
-    private List transactions;
 
-    public TransactionsListVO(List transactions){
+    @JsonView(View.Public.class)
+    private List<ExchangeTransaction> transactions;
+
+    public TransactionsListVO(List<ExchangeTransaction> transactions){
         this.transactions = transactions;
     }
 
-    public List getTransactions(){
+    public List<ExchangeTransaction> getTransactions(){
         return transactions;
     }
 
-    public void setTransactions(List transactions){
+    public void setTransactions(List<ExchangeTransaction> transactions){
         this.transactions = transactions;
     }
 }
