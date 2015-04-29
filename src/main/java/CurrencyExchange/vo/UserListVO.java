c255aac61a7a127f9b87fda71265dd917bf879ed
ivan_ochc <sponsme@ukr.net>
2015-04-29 13:47:32 +0300

    add json view

--- src/main/java/CurrencyExchange/vo/UserListVO.java
+++ src/main/java/CurrencyExchange/vo/UserListVO.java
@@ -1,11 +1,13 @@
 package currencyexchange.vo;
 
+import com.fasterxml.jackson.annotation.JsonView;
 import currencyexchange.model.User;
+import currencyexchange.view.View;
 
 import java.util.List;
 
 public class UserListVO {
-
+    @JsonView(View.Secure.class)
     private List<User> users;
 
     public UserListVO(List<User> users){
