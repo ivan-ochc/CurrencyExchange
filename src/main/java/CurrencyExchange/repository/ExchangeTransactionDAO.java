249ba996dbfa84f5eb50c56723fc018633e93a91
ivan_ochc <sponsme@ukr.net>
2015-04-29 13:42:25 +0300

    delete some methods

--- src/main/java/CurrencyExchange/repository/ExchangeTransactionDAO.java
+++ src/main/java/CurrencyExchange/repository/ExchangeTransactionDAO.java
@@ -7,9 +7,8 @@ import java.util.List;
 public interface ExchangeTransactionDAO {
 
     public void addTransaction(ExchangeTransaction exchangeTransaction);
-    public List getTransactions(String userName);
+    public List<ExchangeTransaction> getTransactions(String userName);
     public void declineTransaction(ExchangeTransaction exchangeTransaction);
-    public void acceptTransaction();
     public ExchangeTransaction getExchangeTransaction(int transactionId);
 
 
